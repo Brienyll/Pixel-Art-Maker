a68e4ca7d159384149b20caac1dab7e7f28d6ea7
Brienyll <brienyll29@gmail.com>
2018-03-25 15:04:23 -0700

    submit button works

--- designs.js
+++ designs.js
@@ -1,9 +1,33 @@
 
+function makeGrid() {    
 // Select color input
-
 // Select size input
+let height, weight, rows, canvas, cell;
+
+canvas = $('#pixelCanvas');
 height = $("#inputHeight").val();
-weight = $("#inputWeight").val();
+width = $("#inputWidth").val();
+
+canvas.children().remove();
+
+for (x = 0; x < height; x++) {
+	canvas.append('<tr></tr>');
+    }
+
+rows = $('tr');    
+
+for (y = 0; y < width; y++) {
+    rows.append('<td></td>');
+    } 
+cell = canvas.find('td');
+
+cell.click(function() {
+    // Select color input
+    let color;
+    color = $("#colorPicker").val();
+    $(this).attr('bgcolor', color);
+});
+}
 
 const submitB = $("input[type='submit']");
 // When size is submitted by the user, call makeGrid()
@@ -12,7 +36,3 @@ submitB.click(function(e){
     makeGrid();
 });
 // Your code goes here!
-
-function makeGrid() {
-    console.log(height);
-    }
